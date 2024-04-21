15a1126fca72f7579e46d545aa352027168e570b
MaTTalv001 <pomeranian1981@gmail.com>
2024-04-22 07:42:28 +0900

    bootstrapで見た目を調整

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,15 @@ module ApplicationHelper
     def user_signed_in?
       current_user.present?
     end
+
+    def bootstrap_class_for(flash_type)
+        case flash_type.to_sym
+        when :notice
+          "alert-info"  # または 'alert-success' など、お好みのアラートタイプ
+        when :alert
+          "alert-warning"  # または 'alert-danger'
+        else
+          flash_type.to_s
+        end
+      end
 end
