7ea485c2cf2a441d8b31c8f2213f0bba84e59d05
MaTTalv001 <pomeranian1981@gmail.com>
2024-04-23 07:51:25 +0900

    音声入力に対応

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -20,4 +20,37 @@ document.addEventListener('turbo:load', function() {
     }
   });
   
+  document.addEventListener("turbo:load", () => {
+    var speechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    if (speechRecognition) {
+      var recognition = new speechRecognition();
+      var textField = document.getElementById('answer_field'); // IDで要素を取得
+      var startButton = document.getElementById('start-speech'); // 音声入力開始ボタンのID
+  
+      recognition.continuous = false; // 一度の認識で止まるように設定
+      recognition.lang = 'ja-JP'; // 日本語に設定
+      recognition.interimResults = false; // 中間結果は不要
+      recognition.maxAlternatives = 1; // 複数の認識結果は不要
+  
+      startButton.addEventListener('click', () => {
+        recognition.start(); // ボタンクリックで認識を開始
+      });
+  
+      recognition.addEventListener('result', (event) => {
+        var speechResult = event.results[0][0].transcript; // 認識結果の取得
+        textField.value = speechResult; // テキストエリアに入力
+      });
+  
+      recognition.addEventListener('speechend', () => {
+        recognition.stop(); // 認識を終了
+      });
+  
+      recognition.addEventListener('error', (event) => {
+        console.error('音声認識エラー: ' + event.error); // エラー処理
+      });
+    } else {
+      console.error("このブラウザはWeb Speech APIをサポートしていません。");
+    }
+  });
+  
   
