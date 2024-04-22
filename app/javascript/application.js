17fa7ceb941de338490ec4cac464cb58207b59e6
MaTTalv001 <pomeranian1981@gmail.com>
2024-04-22 12:24:37 +0900

    リポジトリ空欄時にアラート表示

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,16 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener('turbo:load', function() {
+    const form = document.querySelector('.ai-interview-form');
+    if (form) {
+      form.addEventListener('submit', function(event) {
+        const select = document.querySelector('#repository_select');
+        if (select && select.value === "") {
+          event.preventDefault(); // フォームの送信を停止
+          alert('リポジトリを選択してください。');
+        }
+      });
+    }
+  });
